08f4526494f22bdb245c7587efb1b522550d757e
despiegk <>
2023-10-09 08:17:03 +0300

    ...

---
+++ main.go
@@ -0,0 +1,452 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	log.SetOutput(io.Discard)
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+
+	app := &cli.App{
+		Name:  "grid",
+		Usage: "Example: grid [COMMAND]",
+		Commands: []cli.Command{
+			{
+				Name:  "new-node-cn",
+				Usage: "create node contract",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "node_id",
+						Required: true,
+						Usage:    "node id to create the contract on",
+					},
+					cli.StringFlag{
+						Name:  "body",
+						Value: "",
+						Usage: "contract body",
+					},
+					cli.StringFlag{
+						Name:     "hash",
+						Required: true,
+						Usage:    "deployment hash",
+					},
+					cli.UintFlag{
+						Name:  "public_ips",
+						Value: 0,
+						Usage: "number of reserved public ips for this deployment",
+					},
+					cli.Uint64Flag{
+						Name:  "solution_provider",
+						Value: 0,
+						Usage: "twin id for the solution provider",
+					},
+				},
+				Action: substrateDecorator(createNodeContract),
+			},
+			{
+				Name:  "update-cn",
+				Usage: "update node contract",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.UintFlag{
+						Name:     "contract_id",
+						Required: true,
+						Usage:    "id of contract to update",
+					},
+					cli.StringFlag{
+						Name:  "body",
+						Value: "",
+						Usage: "contract body",
+					},
+					cli.StringFlag{
+						Name:     "hash",
+						Required: true,
+						Usage:    "deployment hash",
+					},
+				},
+				Action: substrateDecorator(updateNodeContract),
+			},
+			{
+				Name:  "cancel-cn",
+				Usage: "cancel any type of contract",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.UintFlag{
+						Name:     "contract_id",
+						Required: true,
+						Usage:    "id of contract to delete",
+					},
+				},
+				Action: substrateDecorator(cancelContract),
+			},
+			{
+				Name:  "new-name-cn",
+				Usage: "create name contract",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.StringFlag{
+						Name:     "name",
+						Required: true,
+						Usage:    "contract name",
+					},
+				},
+				Action: substrateDecorator(createNameContract),
+			},
+			{
+				Name:  "new-rent-cn",
+				Usage: "create rent contract",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.UintFlag{
+						Name:     "node_id",
+						Required: true,
+						Usage:    "id of node to rent",
+					},
+					cli.UintFlag{
+						Name:  "solution_provider",
+						Value: 0,
+						Usage: "solution provider twin id",
+					},
+				},
+				Action: substrateDecorator(createRentContract),
+			},
+			{
+				Name:  "sign",
+				Usage: "sign a deployment",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.StringFlag{
+						Name:     "hash",
+						Required: true,
+						Usage:    "deployment hash",
+					},
+				},
+				Action: substrateDecorator(signDeployment),
+			},
+			{
+				Name:  "node-twin",
+				Usage: "get node twin id",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+					cli.StringFlag{
+						Name:     "node_id",
+						Required: true,
+						Usage:    "node id",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					substrateURL := c.String("substrate")
+					nodeId := c.Uint("node_id")
+					return getNodeTwin(c, substrateURL, uint32(nodeId))
+				},
+			},
+			{
+				Name:  "user-twin",
+				Usage: "get user twin id",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "substrate",
+						Value: "wss://tfchain.grid.tf/ws",
+						Usage: "substrate URL",
+					},
+				},
+				Action: substrateDecorator(getUserTwin),
+			},
+			{
+				Name:  "rmb-dl-deploy",
+				Usage: "call rmb func",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "substrate",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "substrate URL",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "relay",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "relay URL",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "dst",
+						Value:    0,
+						Usage:    "destination",
+						Required: true,
+					},
+
+					cli.StringFlag{
+						Name:     "data",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "Data to be sent",
+						Required: true,
+					},
+				},
+				Action: rmbDecorator(deploymentDeploy),
+			},
+			{
+				Name:  "rmb-dl-get",
+				Usage: "call rmb func",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "substrate",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "substrate URL",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "relay",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "relay URL",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "dst",
+						Value:    0,
+						Usage:    "destination",
+						Required: true,
+					},
+
+					cli.StringFlag{
+						Name:     "data",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "Data to be sent",
+						Required: true,
+					},
+				},
+				Action: rmbDecorator(deploymentGet),
+			},
+			{
+				Name:  "rmb-dl-changes",
+				Usage: "call rmb func",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "substrate",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "substrate URL",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "relay",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "relay URL",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "dst",
+						Value:    0,
+						Usage:    "destination",
+						Required: true,
+					},
+
+					cli.StringFlag{
+						Name:     "contract_id",
+						Value:    "0",
+						Usage:    "contract id to get changes for",
+						Required: true,
+					},
+				},
+				Action: rmbDecorator(deploymentChanges),
+			},
+			{
+				Name:  "rmb-taken-ports",
+				Usage: "call rmb func",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "substrate",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "substrate URL",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "relay",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "relay URL",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "dst",
+						Value:    0,
+						Usage:    "destination node",
+						Required: true,
+					},
+				},
+				Action: rmbDecorator(nodeTakenPorts),
+			},
+			{
+				Name:  "rmb-node-pubConfig",
+				Usage: "Get node public configuration",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "substrate",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "substrate URL",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "relay",
+						Value:    "wss://tfchain.grid.tf/ws",
+						Usage:    "relay URL",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "dst",
+						Value:    0,
+						Usage:    "destination node",
+						Required: true,
+					},
+				},
+				Action: rmbDecorator(getNodePublicConfig),
+			},
+			{
+				Name:  "deploy-single",
+				Usage: "Get node public configuration",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "env",
+						Value:    "main",
+						Usage:    "network env",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "mnemonics",
+						Value:    "",
+						Usage:    "user mnemonics",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "data",
+						Value:    "",
+						Usage:    "vm data",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "solution_type",
+						Value:    "",
+						Usage:    "solution type",
+						Required: true,
+					},
+					cli.UintFlag{
+						Name:     "node",
+						Value:    0,
+						Usage:    "node id",
+						Required: true,
+					},
+				},
+				Action: deployVM(),
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
---
+++ rmb.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	substrate "github.com/threefoldtech/tfchain/clients/tfchain-client-go"
+	"github.com/threefoldtech/tfgrid-sdk-go/rmb-sdk-go/direct"
+	"github.com/threefoldtech/zos/pkg"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/urfave/cli"
+)
+
+type rmbCmdArgs map[string]interface{}
+
+func rmbDecorator(action func(c *cli.Context, client *direct.RpcCLient) (interface{}, error)) cli.ActionFunc {
+	return func(c *cli.Context) error {
+		substrate_url := c.String("substrate")
+		mnemonics := c.String("mnemonics")
+		relay_url := c.String("relay")
+
+		subManager := substrate.NewManager(substrate_url)
+		sub, err := subManager.Substrate()
+		if err != nil {
+			return fmt.Errorf("failed to connect to substrate: %w", err)
+		}
+		defer sub.Close()
+		client, err := direct.NewRpcClient(context.Background(), direct.KeyTypeSr25519, mnemonics, relay_url, "tfgrid-vclient", sub, true)
+
+		if err != nil {
+			return fmt.Errorf("failed to create direct client: %w", err)
+		}
+
+		res, err := action(c, client)
+
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%v", res)
+		return nil
+
+	}
+}
+
+func deploymentChanges(c *cli.Context, client *direct.RpcCLient) (interface{}, error) {
+	dst := uint32(c.Uint("dst"))
+	contractID := c.Uint64("contract_id")
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	var changes []gridtypes.Workload
+	args := rmbCmdArgs{
+		"contract_id": contractID,
+	}
+	err := client.Call(ctx, dst, "zos.deployment.changes", args, &changes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get deployment changes after deploy: %w, contractID: %d", err, contractID)
+	}
+	res, err := json.Marshal(changes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal deployment changes%w", err)
+	}
+	return string(res), nil
+}
+
+func deploymentDeploy(c *cli.Context, client *direct.RpcCLient) (interface{}, error) {
+	dst := uint32(c.Uint("dst"))
+	data := c.String("data")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	var dl gridtypes.Deployment
+	err := json.Unmarshal([]byte(data), &dl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal deployment %w", err)
+	}
+
+	if err := client.Call(ctx, dst, "zos.deployment.deploy", dl, nil); err != nil {
+		return nil, fmt.Errorf("failed to deploy deployment %w", err)
+	}
+
+	return nil, nil
+}
+
+func deploymentGet(c *cli.Context, client *direct.RpcCLient) (interface{}, error) {
+	dst := uint32(c.Uint("dst"))
+	data := c.String("data")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	var args rmbCmdArgs
+	err := json.Unmarshal([]byte(data), &args)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal data to get deployment %w", err)
+	}
+	var dl gridtypes.Deployment
+
+	if err := client.Call(ctx, dst, "zos.deployment.get", args, &dl); err != nil {
+		return nil, fmt.Errorf("failed to get deployment %w", err)
+	}
+	json, err := json.Marshal(dl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal deployment %w", err)
+	}
+
+	return string(json), nil
+}
+
+func nodeTakenPorts(c *cli.Context, client *direct.RpcCLient) (interface{}, error) {
+	dst := uint32(c.Uint("dst"))
+	var takenPorts []uint16
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := client.Call(ctx, dst, "zos.network.list_wg_ports", nil, &takenPorts); err != nil {
+		return nil, fmt.Errorf("failed to get node taken ports %w", err)
+	}
+	json, err := json.Marshal(takenPorts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal taken ports %w", err)
+	}
+
+	return string(json), nil
+}
+
+func getNodePublicConfig(c *cli.Context, client *direct.RpcCLient) (interface{}, error) {
+	dst := uint32(c.Uint("dst"))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	var pubConfig pkg.PublicConfig
+
+	if err := client.Call(ctx, dst, "zos.network.public_config_get", nil, &pubConfig); err != nil {
+		return nil, fmt.Errorf("failed to get node public configuration: %w", err)
+	}
+	json, err := json.Marshal(pubConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal public configuration: %w", err)
+	}
+	fmt.Println(string(json))
+	return string(json), nil
+}
